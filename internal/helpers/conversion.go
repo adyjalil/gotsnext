1150098e50bd82e8cc49b7a9014e77a1db0cc250
ady jalil <ady.s.jalil@gmail.com>
2021-10-14 21:47:00 +0800

    Upload

--- internal/helpers/conversion.go
+++ internal/helpers/conversion.go
@@ -24,3 +24,34 @@ var Difficulty = struct {
 	"medium",
 	"hard",
 }
+
+type square struct {
+	length int
+	height int
+}
+
+type triangle struct {
+	base   int
+	height int
+}
+
+func (s *square) getArea() float64 {
+	return float64(s.length) * float64(s.height)
+}
+
+func (t *triangle) getArea() float64 {
+	return float64(t.base) * float64(t.height) / 2
+}
+
+type shape interface {
+	getArea() float64
+}
+
+func draw(s shape) {
+	//draw
+	s.getArea()
+}
+
+func (s *square) Read(p []byte) (n int, err error) {
+	return 0, nil
+}
--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"gotsnext/internal/helpers"
+	"io"
+	"log"
+	"net/http"
+	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+	file, err := os.Open("a.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
 
+	io.Copy(os.Stdout, file)
 	conf := fiber.Config{
 		ServerHeader: "go fiber",
 	}
 	app := fiber.New(conf)
 
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World!")
+		return c.SendFile("./html/index.html", true)
 	})
 
 	app.Get("/n/:number", func(c *fiber.Ctx) error {
@@ -40,5 +52,57 @@ func main() {
 		return c.SendString(fmt.Sprintf("number value: %d", n))
 	})
 
+	app.Post("/file", func(c *fiber.Ctx) error {
+
+		fh, err := c.FormFile("file")
+		if err != nil {
+			return c.Status(fiber.ErrBadRequest.Code).JSON(err.Error())
+		}
+		file, err := fh.Open()
+		if err != nil {
+			return c.Status(fiber.ErrBadRequest.Code).JSON("error open")
+		}
+		defer file.Close()
+
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(file)
+
+		tp := http.DetectContentType(buf.Bytes())
+		name := fh.Filename
+		size := fh.Size
+
+		res := struct {
+			Type         string `json:"type"`
+			Name         string `json:"name"`
+			Size         int64  `json:"size"`
+			Message      string `json:"message"`
+			UploadedSize int    `json:"uploaded_size"`
+			Filename     string `json:"filename"`
+		}{
+			Type:    tp,
+			Name:    name,
+			Size:    size,
+			Message: "upload successful",
+		}
+		suffix := ".png"
+		if strings.Contains(tp, "jpeg") {
+			suffix = ".jpg"
+		}
+		fname := fmt.Sprintf("./assets/img_%d%s", time.Now().Unix(), suffix)
+		dst, err := os.Create(fname)
+		if err != nil {
+			return c.Status(fiber.ErrInternalServerError.Code).JSON(err.Error())
+		}
+		defer dst.Close()
+		res.UploadedSize, err = dst.Write(buf.Bytes())
+		res.Filename = fname
+		if err != nil {
+			return c.Status(fiber.ErrInternalServerError.Code).JSON(err.Error())
+		}
+
+		return c.JSON(res)
+	})
+
+	app.Static("/", "./assets")
 	app.Listen(":3000")
 }
